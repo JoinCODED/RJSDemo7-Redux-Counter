9853931d32967977b49081517183eb627791a25a
Octowl <alsaff1987@gmail.com>
2019-02-03 20:50:02 +0300

    new syntax

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -2,17 +2,19 @@ import * as actionTypes from "../actions/actionTypes";
 import axios from "axios";
 
 export const fetchPosts = () => {
-  return dispatch => {
+  return async dispatch => {
     //This function gets called by Redux Thunk
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.data)
-      .then(posts =>
-        dispatch({
-          //Once we recieve a response, dispatch the action
-          type: actionTypes.FETCH_POSTS,
-          payload: posts
-        })
-      );
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const posts = res.data;
+
+      dispatch({
+        //Once we recieve a response, dispatch the action
+        type: actionTypes.FETCH_POSTS,
+        payload: posts
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
 };
