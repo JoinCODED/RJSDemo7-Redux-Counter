2520aab703fe355eab7724755e4500eb16d84c02
Octowl <alsaff1987@gmail.com>
2019-02-03 17:12:12 +0300

    Actually bring back redux

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,25 +1,23 @@
-
 const initialState = {
-    copyMe: "Remember to copy me!",
-    counter: 0,
-}
+  copyMe: "Remember to copy me!",
+  counter: 0
+};
 
-const reducer = (state=initialState, action) => {
-    switch(action.type){
-        case 'INCREMENT':
-            return {
-                ...state,
-                counter: state.counter + 1, //copy all of the old state, and only manipulate the part we want to manipulate.
-            }
-        case 'DECREMENT':
-            return {
-                ...state,
-                counter: state.counter - 1,
-            }
-        default:
-            return state;
-    }
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1 //copy all of the old state, and only manipulate the part we want to manipulate.
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
-
